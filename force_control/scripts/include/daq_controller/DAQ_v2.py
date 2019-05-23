b731ce3b212893d79fe53cb5539fdac0d78169f7
sergiosierram <sergiod.sierram19@gmail.com>
2019-05-23 14:09:49 -0500

    Updating plx_walker_force

--- force_control/scripts/include/daq_controller/DAQ_v2.py
+++ force_control/scripts/include/daq_controller/DAQ_v2.py
@@ -11,14 +11,14 @@ class DAQ_v2(object):
         self.m = 22.95
         self.b = 0.25
         ## C
-        self.channels = (0,1,2,3)
+        self.channels = (3,2,7,6)
 
     def get_forces(self):
         channels_info = self.NIDevice.readchannel(self.channels)
-        ly_force = channels_info[0][0]*self.m + self.b
-        lz_force = channels_info[0][1]*self.m + self.b
-        ry_force = channels_info[0][2]*self.m + self.b
-        rz_force = channels_info[0][3]*self.m + self.b
+        ry_force = channels_info[0][0]*self.m + self.b
+        rz_force = channels_info[0][1]*self.m + self.b
+        lz_force = channels_info[0][2]*self.m + self.b
+        ly_force = channels_info[0][3]*self.m + self.b
         #print(channels_info)
         return ("{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}".format(ly_force, lz_force, ry_force, rz_force))
 
